b55b5e58da4d234081f0f109930ee0ae5d45f0c4
lex <362613697@qq.com>
2022-02-24 15:40:31 +0800

    'update'

---
+++ tlf/js/main.js
@@ -0,0 +1,31 @@
+$(function(){
+	var caseList = [
+		[
+			{name:'周生生幸运捉福娃',img:'case1.png',link:'case1'},
+		],
+		[],
+		[],
+		[],
+		[],
+		[],
+		[]
+	]
+	
+	$(".nav li").click(function(){
+		$(this).addClass("active").siblings().removeClass("active");
+		var thisnum = $(this).index();
+		var typename = $(this).find('a').text();
+		var html = '';
+		for(var i=0;i<caseList[thisnum].length;i++){
+			html+=`<div class="box2 col-xs-12 col-sm-6 col-md-4 wow animate fadeInDown animated" style="visibility: visible; animation-name: fadeInDown;">
+						<a target="_blank" href="pdf.html?p=${caseList[thisnum][i].link}" class="thumbnail">
+							<img src="img/${caseList[thisnum][i].img}" alt="${typename}">
+							<p class="mask">${typename}</p>
+						</a>
+						<div class="casename">${caseList[thisnum][i].name}</div>
+					</div>`
+		}
+		$(".caseList").html(html);
+	})
+	$(".nav li").eq(0).click();
+})
